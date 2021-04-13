c39337c97447c7b7a09255d6e362be6e853bea2a
santiago <smelomedina05@gmail.com>
2021-04-13 16:30:32 -0500

    configured some values

--- src/constant/connection.py
+++ src/constant/connection.py
@@ -4,7 +4,7 @@ import os
 env = os.environ
 
 PORT = env.get("PORT") if env.get("PORT") else "5000"
-DB_HOST = env.get("DB_HOST") if env.get("DB_HOST") else "172.17.0.2"
+DB_HOST = env.get("DB_HOST") if env.get("DB_HOST") else "localhost"
 DB_PORT = env.get("DB_PORT") if env.get("DB_PORT") else "3306"
 DB_USER = env.get("DB_USER") if env.get("DB_USER") else "root"
 DB_NAME = env.get("DB_NAME") if env.get("DB_NAME") else "Test"
